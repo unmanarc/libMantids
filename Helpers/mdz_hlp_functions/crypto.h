8765ebf0ec0454c5738fb062078649fd4af6b9df
Aaron Mizrachi <aaron@unmanarc.com>
2023-06-14 09:29:12 +0300

    new function to use 96-bit crypto IV in AES-GCM
    - this should solve compatibility issues but it's not backward
    compatible with the 128bit IV function.

--- Helpers/mdz_hlp_functions/crypto.h
+++ Helpers/mdz_hlp_functions/crypto.h
@@ -19,9 +19,20 @@ public:
     static std::string AES256EncryptB64(const std::string & input, const char *key, uint32_t keyLen, bool * ok);
     static std::string AES256EncryptB64(const std::string & input, const std::string & key, bool * ok);
 
+
+
+    static std::string AES256EncryptB64v2(const unsigned char * input, uint32_t inputLen, const char * key, uint32_t keyLen, bool * ok);
+    static std::string AES256EncryptB64v2(const std::string & input, const char *key, uint32_t keyLen, bool * ok);
+    static std::string AES256EncryptB64v2(const std::string & input, const std::string & key, bool * ok);
+
+
     static std::shared_ptr<Mem::xBinContainer> AES256DecryptB64ToBin(const std::string & input, const char *key, uint32_t keyLen, bool * ok);
     static std::string AES256DecryptB64(const std::string & input, const char *key, uint32_t keyLen, bool * ok);
     static std::string AES256DecryptB64(const std::string & input, const std::string & key, bool * ok);
+
+    static std::shared_ptr<Mem::xBinContainer> AES256DecryptB64ToBinv2(const std::string & input, const char *key, uint32_t keyLen, bool * ok);
+    static std::string AES256DecryptB64v2(const std::string & input, const char *key, uint32_t keyLen, bool * ok);
+    static std::string AES256DecryptB64v2(const std::string & input, const std::string & key, bool * ok);
 
     static std::string calcSHA256(const std::string & password);
     static std::string calcSHA512(const std::string & password);
@@ -29,6 +40,11 @@ public:
     // SALT must be 4 bytes.
     static std::string calcSSHA256(const std::string & password, const unsigned char *ssalt);
     static std::string calcSSHA512(const std::string & password, const unsigned char *ssalt);
+
+
+private:
+    static std::string AES256EncryptB64_v0(const unsigned char * input, uint32_t inputLen, const char * key, uint32_t keyLen, int ivLength, bool * ok);
+    static std::shared_ptr<Mem::xBinContainer> AES256DecryptB64ToBin_v0(const std::string & input, const char *key, uint32_t keyLen, int ivLength, bool * ok);
 };
 
 }}
